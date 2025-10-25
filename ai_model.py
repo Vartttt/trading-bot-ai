33710ef457361832cc0516c27ae9cce4dc3156ee
Viktor <tasiukviktor@gmail.com>
2025-10-25 22:54:48 +0200

    Update ai_model.py

--- ai_model.py
+++ ai_model.py
@@ -2,15 +2,14 @@ import numpy as np
 from sklearn.ensemble import GradientBoostingClassifier
 import joblib
 import os
-
-MODEL_PATH = "ai_model.pkl"
+from config import MODEL_PATH
 
 class AIMarketModel:
     def __init__(self):
         if os.path.exists(MODEL_PATH):
             self.model = joblib.load(MODEL_PATH)
         else:
-            self.model = GradientBoostingClassifier(n_estimators=100, max_depth=3)
+            self.model = GradientBoostingClassifier(n_estimators=120, max_depth=3)
 
     def predict_signal(self, X: np.ndarray) -> dict:
         if len(X.shape) == 1:
@@ -19,7 +18,6 @@ class AIMarketModel:
         return {"up": prob[1], "down": prob[0]}
 
     def fit_online(self, X, y):
-        # Псевдо оновлення: refit на об'єднаних даних (спрощений онлайн)
         try:
             self.model.fit(X, y)
             joblib.dump(self.model, MODEL_PATH)
