81caafb1d6b0cb086b8bc4cb28b5c099fb900a8d
Viktor <tasiukviktor@gmail.com>
2025-10-25 21:54:00 +0200

    Update learner.py

--- learner.py
+++ learner.py
@@ -1,1 +1,14 @@
+import numpy as np
+from ai_model import AIMarketModel
+from feature_engineering import build_features
+
+ai_model = AIMarketModel()
+
+def record_trade_result(df, side, result):
+    """
+    result: 1 якщо TP, 0 якщо SL
+    """
+    X = build_features(df)
+    y = np.array([result])
+    ai_model.fit_online(X.reshape(1,-1), y)
 
