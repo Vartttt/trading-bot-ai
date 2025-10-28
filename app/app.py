ac1bf337a5e60e613a2c99f14a72dfbdac6ea990
Viktor <tasiukviktor@gmail.com>
2025-10-28 08:03:17 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-import os, time, json, threading
 from flask import Flask, jsonify, Response
 from prometheus_client import Gauge, Counter, generate_latest, CONTENT_TYPE_LATEST
 
