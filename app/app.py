ec5743f6973c6f90412caa29054182c3f8442559
Viktor <tasiukviktor@gmail.com>
2025-11-03 19:55:20 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,3 +1,12 @@
+import os
+import sys
+
+# Додаємо корінь проєкту до системного шляху
+current_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.abspath(os.path.join(current_dir, ".."))
+if root_dir not in sys.path:
+    sys.path.append(root_dir)
+
 # ==============================
 # ✅ UNIVERSAL IMPORT & MODEL_DIR HANDLER
 # ==============================
